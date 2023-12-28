f5093b2c8ed0baa1046ab2a0495995ad7695107a
Md. Al-Amin <145653719+mdalamin006@users.noreply.github.com>
2023-12-28 18:36:47 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// JavaScript code 
+
+
+
+
+// jQurey Code
+$(document).ready(function () {
+  $('.contents').btnLoadmore({
+    showItem : 3,
+    whenClickBtn : 3,
+    textBtn : 'Load more...',
+    classBtn : 'btn btn-danger mt-3'
+});
+
+});
